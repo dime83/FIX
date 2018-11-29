ef8ee624372e3781223b6b1c4fa428f23ef9b731
dime83 <david.manio@gmail.com>
2018-11-29 23:20:14 +0000

    All Working

--- fix.py
+++ fix.py
@@ -1,5 +1,13 @@
 import csv
 
+# Open FIX mappings and add them to the dictionary
+with open('fixMappings.csv') as fm:
+    reader = csv.DictReader(fm)
+    tagFields ={}
+    for row in reader:
+        tagFields[row['Tag']] = row['Field']
+
+# Open your FIX messages put each row into a dictionary and each dictionary into a list of all rows
 with open('fix.csv') as mycsv:
     csv_reader = csv.reader(mycsv, delimiter=",")
     fixDict=[]
@@ -8,7 +16,7 @@ with open('fix.csv') as mycsv:
         for kvpair in row:
             #Split our FIX Value pair to key and value
             k,v = kvpair.split("=")
-            my_dict[k] = v    
+            my_dict[tagFields[k]] = v    
         fixDict.append(my_dict)
 
     for element in fixDict:
@@ -16,7 +24,7 @@ with open('fix.csv') as mycsv:
 mycsv.close()
 
 with open('convertedfix.csv', mode='w') as converted_fix:
-    fieldnames = ['40','55','38']
+    fieldnames = ['MsgType','Symbol','OrdType']
     writer = csv.DictWriter(converted_fix,fieldnames=fieldnames,extrasaction='ignore')
 
     writer.writeheader()
